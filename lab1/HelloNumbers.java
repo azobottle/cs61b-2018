9c8dfeffa897655f1f64e90d666295941d8823a4
Lee <1553359439@qq.com>
2022-01-23 00:05:10 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int ans=0;
+        for(int i=0;i<10;i++){
+            ans+=i;
+            System.out.print(ans+" ");//???
+        }
+    }
+}
