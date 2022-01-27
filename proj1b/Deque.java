7218b2a840b09064619e12785c8dc6abbe34c4dc
azobottle <1553359439@qq.com>
2022-01-27 22:39:19 +0800

    try

---
+++ proj1b/Deque.java
@@ -0,0 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public interface Deque<T> {
+    void addFirst(T var1);
+
+    void addLast(T var1);
+
+    boolean isEmpty();
+
+    int size();
+
+    void printDeque();
+
+    T removeFirst();
+
+    T removeLast();
+
+    T get(int var1);
+}
