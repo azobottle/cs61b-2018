18fe6d7715012bb02afd8dc8143ce817cd3d174e
azobottle <1553359439@qq.com>
2022-02-02 21:02:31 +0800

    temp

--- hw1/GuitarHeroLite.java
+++ hw1/GuitarHeroLite.java
@@ -1,10 +1,19 @@
-/** A client that uses the synthesizer package to replicate a plucked guitar string sound */
+import synthesizer.GuitarString;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
 public class GuitarHeroLite {
     private static final double CONCERT_A = 440.0;
     private static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
+    private static String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static synthesizer.GuitarString[] strings = new GuitarString[37];
 
     public static void main(String[] args) {
         /* create two guitar strings, for concert A and C */
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = new GuitarString(440 * Math.pow(2, (double) (i - 24) / 12));
+        }
         synthesizer.GuitarString stringA = new synthesizer.GuitarString(CONCERT_A);
         synthesizer.GuitarString stringC = new synthesizer.GuitarString(CONCERT_C);
 
@@ -13,20 +22,20 @@ public class GuitarHeroLite {
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                if (key == 'a') {
-                    stringA.pluck();
-                } else if (key == 'c') {
-                    stringC.pluck();
+                if (keyboard.indexOf(key) >= 0) {
+
+                } else {
+                    continue;
                 }
             }
 
-        /* compute the superposition of samples */
+            /* compute the superposition of samples */
             double sample = stringA.sample() + stringC.sample();
 
-        /* play the sample on standard audio */
+            /* play the sample on standard audio */
             StdAudio.play(sample);
 
-        /* advance the simulation of each guitar string by one step */
+            /* advance the simulation of each guitar string by one step */
             stringA.tic();
             stringC.tic();
         }
