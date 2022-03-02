8a4550160e4dc51904013b78b07762012a196e24
az0bottle <1553359439@qq.com>
2022-03-02 14:45:32 +0800

    hw4-BFS-puzzle solver --modify equal

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -104,7 +104,7 @@ public class Board implements WorldState {
         }
         Board b = (Board) y;
         int N = this.size();
-        if (N != this.tiles[0].length) {
+        if (N != b.size()) {
             return false;
         }
         for (int i = 0; i < N; i++) {
