032d5474d1abc3de9e16af8108b488aac5ba85f4
az0bottle <1553359439@qq.com>
2022-03-05 00:13:01 +0800

    proj3-need to fix Rasterer.java

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,29 +8,32 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+    private double MLonDPP;
+    private double MlatDPP;
 
     public Rasterer() {
+        MLonDPP = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / 256;
+        MlatDPP = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / 256;
         // YOUR CODE HERE
     }
 
     /**
      * Takes a user query and finds the grid of images that best matches the query. These
      * images will be combined into one big image (rastered) by the front end. <br>
-     *
-     *     The grid of images must obey the following properties, where image in the
-     *     grid is referred to as a "tile".
-     *     <ul>
-     *         <li>The tiles collected must cover the most longitudinal distance per pixel
-     *         (LonDPP) possible, while still covering less than or equal to the amount of
-     *         longitudinal distance per pixel in the query box for the user viewport size. </li>
-     *         <li>Contains all tiles that intersect the query bounding box that fulfill the
-     *         above condition.</li>
-     *         <li>The tiles must be arranged in-order to reconstruct the full image.</li>
-     *     </ul>
+     * <p>
+     * The grid of images must obey the following properties, where image in the
+     * grid is referred to as a "tile".
+     * <ul>
+     *     <li>The tiles collected must cover the most longitudinal distance per pixel
+     *     (LonDPP) possible, while still covering less than or equal to the amount of
+     *     longitudinal distance per pixel in the query box for the user viewport size. </li>
+     *     <li>Contains all tiles that intersect the query bounding box that fulfill the
+     *     above condition.</li>
+     *     <li>The tiles must be arranged in-order to reconstruct the full image.</li>
+     * </ul>
      *
      * @param params Map of the HTTP GET request's query parameters - the query box and
      *               the user viewport width and height.
-     *
      * @return A map of results for the front end as specified: <br>
      * "render_grid"   : String[][], the files to display. <br>
      * "raster_ul_lon" : Number, the bounding upper left longitude of the rastered image. <br>
@@ -39,14 +42,83 @@ public class Rasterer {
      * "raster_lr_lat" : Number, the bounding lower right latitude of the rastered image. <br>
      * "depth"         : Number, the depth of the nodes of the rastered image <br>
      * "query_success" : Boolean, whether the query was able to successfully complete; don't
-     *                    forget to set this to true on success! <br>
+     * forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        double h = params.get("h"), w = params.get("w"), lrlat = params.get("lrlat"),
+                lrlon = params.get("lrlon"),
+                ullat = params.get("ullat"), ullon = params.get("ullon");
+        /*if (ullon < MapServer.ROOT_ULLON || ullat > MapServer.ROOT_ULLAT ||
+                lrlon > MapServer.ROOT_LRLON || lrlat < MapServer.ROOT_LRLAT) {
+            results.put("query_success", false);
+            return results;
+        } else {
+            results.put("query_success", true);
+        }*/
+        int dep = calDepth((lrlon - ullon) / w);
+        results.put("depth", dep);
+        double a_LonDPP = MLonDPP * Math.pow(0.5, dep), a_LatDPP = MlatDPP * Math.pow(0.5, dep);
+        int ul_x = -1, ul_y = -1, lr_x = -1, lr_y = -1, len = (int) Math.pow(2, dep);
+        for (int i = 0; i < len; i++) {
+            if ((MapServer.ROOT_ULLON + a_LonDPP * 256 * i) <= ullon &&
+                    ullon < (MapServer.ROOT_ULLON + a_LonDPP * 256 * (i + 1))) {
+                results.put("raster_ul_lon", MapServer.ROOT_ULLON + a_LonDPP * 256 * i);
+                ul_x = i;
+            }
+            if ((MapServer.ROOT_ULLON + a_LonDPP * 256 * i) < lrlon
+                    && lrlon <= (MapServer.ROOT_ULLON + a_LonDPP * 256 * (i + 1))) {
+                results.put("raster_lr_lon", MapServer.ROOT_ULLON + a_LonDPP * 256 * (i + 1));
+                lr_x = i;
+                break;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if ((MapServer.ROOT_ULLAT - a_LatDPP * 256 * i) >= ullat &&
+                    ullat > (MapServer.ROOT_ULLAT - a_LatDPP * 256 * (i + 1))) {
+                ul_y = i;
+                results.put("raster_ul_lat", MapServer.ROOT_ULLAT - a_LatDPP * 256 * i);
+            }
+            if ((MapServer.ROOT_ULLAT - a_LatDPP * 256 * i) > lrlat &&
+                    lrlat >= (MapServer.ROOT_ULLAT - a_LatDPP * 256 * (i + 1))) {
+                lr_y = i;
+                results.put("raster_lr_lat", MapServer.ROOT_ULLAT - a_LatDPP * 256 * (i + 1));
+                break;
+            }
+        }
+        if (ul_x < 0 || ul_y < 0 || lr_x < 0 || lr_y < 0) {
+            results.put("query_success", false);
+        } else {
+            results.put("query_success", true);
+        }
+        String[][] a = new String[lr_y - ul_y + 1][lr_x - ul_x + 1];
+        for (int i = ul_y; i <= lr_y; i++) {
+            for (int j = ul_x; j <= lr_x; j++) {
+                a[i - ul_y][j - ul_x] = "d" + dep + "_x" + j + "_y" + i + ".png";
+            }
+        }
+        results.put("render_grid", a);
         return results;
     }
 
+    /*private int findCoordinate(double base, double stepsize, double goal1, double goal2,
+                               String s1, String s2, Map<String, Object> result, int len) {
+        for (int i = 0; i < len; i++) {
+            if (goal1 < (base + stepsize * i)) {
+
+            }
+        }
+    }*/
+
+    private int calDepth(double LDPP) {
+        int dep = 0;
+        while (dep < 7) {
+            if (MLonDPP * Math.pow(0.5, dep) <= LDPP) {
+                break;
+            }
+            dep++;
+        }
+        return dep;
+    }
 }
