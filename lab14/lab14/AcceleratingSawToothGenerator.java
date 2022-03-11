9546b65b998e6f1ee60ce5705e218eec64fbf8ea
az0bottle <1553359439@qq.com>
2022-03-11 22:42:08 +0800

    lab14 Fractal Sound

--- lab14/lab14/AcceleratingSawToothGenerator.java
+++ lab14/lab14/AcceleratingSawToothGenerator.java
@@ -3,27 +3,27 @@ package lab14;
 import lab14lib.Generator;
 
 public class AcceleratingSawToothGenerator implements Generator {
-    private int num;
-    private int next_num;
+    private int period;
+    private int next_period;
     private double factor;
     private int state = -1;
 
-    AcceleratingSawToothGenerator(int num, double factor) {
-        this.num = num;
-        next_num = num;
+    AcceleratingSawToothGenerator(int period, double factor) {
+        this.period = period;
+        next_period = period;
         this.factor = factor;
     }
 
     @Override
     public double next() {
-        state = (state + 1) % num;
+        state = (state + 1) % period;
         if (state == 0) {
-            num = next_num;
-            next_num = (int) (num * factor);
-            if (next_num == 0) {
-                next_num = 1;
+            period = next_period;
+            next_period = (int) (period * factor);
+            if (next_period <= 1) {
+                next_period = 2;
             }
         }
-        return (1.0 * state / (num - 1) - 0.5) * 2;
+        return (1.0 * state / (period - 1) - 0.5) * 2;
     }
 }
